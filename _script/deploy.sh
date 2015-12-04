e2cca5e1dc62fa5c40fcc00108f29c1f60a1df1c
Abhay Rana <capt.n3m0@gmail.com>
2015-12-04 12:32:29 +0530

    Tests the deploy script.
    - Pushes from plugins to compiled branch currently

---
+++ _script/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if ([ $TRAVIS_BRANCH == "plugins" ] && [ $TRAVIS_PULL_REQUEST == "false" ])
+then
+
+  # go to the out directory and create a *new* Git repo
+  cd __site
+
+  git init
+
+  # inside this git repo we'll pretend to be a new user
+  git config user.name "HackerCouch Bot"
+  git config user.email "bot@hackercouch.com"
+
+  # The first and only commit to this new Git repo contains all the
+  # files present with the commit message "Deploy to GitHub Pages".
+  git add .
+  git commit -m "Deploy to GitHub Pages"
+
+  # Force push from the current repo's master branch to the remote
+  # repo's gh-pages branch. (All previous history on the gh-pages branch
+  # will be lost, since we are overwriting it.) We redirect any output to
+  # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:compiled > /dev/null 2>&1
+  echo 'Build successful, deployed to gh-pages.'
+else
+  echo "Build successful, but not deploying!"
+fi
