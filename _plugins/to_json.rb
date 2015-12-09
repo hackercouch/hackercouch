60213862eceb5af041dc5b7b2e92c29e5a515f4d
Abhay Rana <capt.n3m0@gmail.com>
2015-12-09 19:12:59 +0530

    Adds a json key in the couches.json response.

--- _plugins/to_json.rb
+++ _plugins/to_json.rb
@@ -6,6 +6,13 @@ module Jekyll
       input.to_json
     end
   end
+  module AddToHashFilter
+    def add_to_hash(input, key, value)
+      input[key] = value
+      input
+    end
+  end
 end
 
 Liquid::Template.register_filter(Jekyll::ToJsonFilter)
+Liquid::Template.register_filter(Jekyll::AddToHashFilter)
