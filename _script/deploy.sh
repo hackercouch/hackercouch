415e915a6bc29dbf09e854fea2474d52d8540da9
Abhay Rana <capt.n3m0@gmail.com>
2015-12-09 18:52:03 +0530

    Push to gh-pages after a build
    - Make sure travis doesn't ignore master

--- _script/deploy.sh
+++ _script/deploy.sh
@@ -23,7 +23,7 @@ then
   # repo's gh-pages branch. (All previous history on the gh-pages branch
   # will be lost, since we are overwriting it.) We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:compiled > /dev/null 2>&1
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
   echo 'Build successful, deployed to gh-pages.'
 else
   echo "Build successful, but not deploying!"
