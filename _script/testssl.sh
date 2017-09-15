4e8c9d890b155079ff5f47fc9b68e65d94ed6f3b
Nemo <me@captnemo.in>
2017-09-16 04:51:49 +0530

    Adds a testssl script

---
+++ _script/testssl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+curl -Ivv https://chaturvedi.me/
+curl -Ivv https://captnemo.in/
+curl -Ivv https://www.engagespark.com/
+curl -Ivv https://abhishekdas.com/
