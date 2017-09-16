16925134c915c596d676afe5635ed27d6e8e7b49
Nemo <me@captnemo.in>
2017-09-16 20:19:01 +0530

    Makes htmlproofer optional

--- _script/testssl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-curl -Ivv https://chaturvedi.me/
-curl -Ivv https://captnemo.in/
-curl -Ivv https://www.engagespark.com/
-curl -Ivv https://abhishekdas.com/
