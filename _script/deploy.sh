94bb111dc0afe5166714d520caa56c0ad5c90ba1
Nemo <commits@captnemo.in>
2022-03-11 14:12:17 +0530

    Configure publishing

--- _script/deploy.sh
+++ _script/deploy.sh
@@ -11,10 +11,14 @@ cd _site
 
 git init --initial-branch=gh-pages
 
+# https://api.github.com/users/github-actions[bot]
+git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config --global user.name "GitHub Actions [Bot]"
+
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
 git commit -m "Deploy to GitHub Pages"
 
 # Publish to GitHub Pages
-git push --force --quiet "https://${GITHUB_TOKEN}@${GH_REF}"
+git push --force --quiet "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@${GH_REF}"
