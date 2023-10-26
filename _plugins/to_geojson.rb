e860f725f2f96f079c4911c652455cc1362b8602
Nemo <commits@captnemo.in>
2023-10-26 10:27:09 +0530

    Fix URL endpoint as per Nominatim API changes
    See https://github.com/osm-search/Nominatim/issues/3134 for discussion.
    
    This also now adds additional params which should make searches faster.

--- _plugins/to_geojson.rb
+++ _plugins/to_geojson.rb
@@ -5,7 +5,7 @@ OpenURI::Cache.cache_path = '_cache'
 
 module Jekyll
   class GeoJsonGenerator <  Generator
-    BASE_URL = 'http://nominatim.openstreetmap.org/search/'
+    BASE_URL = 'http://nominatim.openstreetmap.org/search'
 
     def generate(site)
       result = Hash[
@@ -17,7 +17,14 @@ module Jekyll
         data = couch.data
         country = URI.encode_www_form_component data['country']
         city = URI.encode_www_form_component data['city']
-        url = BASE_URL + "#{country}/#{city}?format=json"
+        # https://nominatim.org/release-docs/develop/api/Search/
+        # free-form search for the name
+        # filter by country
+        # output as json
+        # restrict to 1 response on the address layer, ignoring POIs, transit options etc
+        # and filter by "settlements" (state, cities, towns, villages, neighbourhoods etc)
+        url = BASE_URL + "?q=#{city}&countrycodes=#{country}&format=json&layer=address&featureType=settlement&limit=1"
+        puts url
         response = URI.open(url)
         raise 'Invalid response from nominatim' if response.status[0] != '200'
         response = JSON.parse(response.read)
